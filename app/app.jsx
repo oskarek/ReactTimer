9ad218253e957aa916120709812048c82a036986
Oskar Ek <oskarek24@gmail.com>
2016-06-21 15:32:02 +0200

    Added countdown functionality

--- app/app.jsx
+++ app/app.jsx
@@ -3,7 +3,7 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 var Main = require('Main');
 var Timer = require('Timer');
-var Countdown = require('Countdown');
+var {Countdown} = require('Countdown');
 
 // load foundation
 require('style!css!foundation-sites/dist/foundation.min.css');
--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -2,13 +2,40 @@ var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
 
+var CountdownStatus = {
+  STOPPED: 1,
+  PAUSED: 2,
+  STARTED: 3
+};
+
 var Countdown = React.createClass({
   getInitialState: function () {
-    return {count: 0};
+    return {
+      count: 0,
+      countdownStatus: CountdownStatus.STOPPED
+    };
+  },
+  componentDidUpdate: function (prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch (this.state.countdownStatus) {
+        case CountdownStatus.STARTED:
+          this.startTimer();
+          break;
+        default:
+
+      }
+    }
+  },
+  startTimer: function () {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      this.setState({count: newCount > 0 ? newCount : 0});
+    }, 1000);
   },
   handleSetCountdown: function (seconds) {
     this.setState({
-      count: seconds
+      count: seconds,
+      countdownStatus: CountdownStatus.STARTED
     });
   },
   render: function () {
@@ -22,4 +49,7 @@ var Countdown = React.createClass({
   }
 });
 
-module.exports = Countdown;
+module.exports = {
+  Countdown: Countdown,
+  CountdownStatus: CountdownStatus
+};
